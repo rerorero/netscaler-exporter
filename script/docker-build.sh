597fdc39f22beb28aa42b7a825657ec8b7267522
rerorero <nato9598@hotmail.co.jp>
2018-01-10 16:58:39 +0900

    Added latest tag

--- script/docker-build.sh
+++ script/docker-build.sh
@@ -1,4 +1,7 @@
-tag="$1"
+image="$1"
+tag="$2"
 docker --version
-docker build -t $tag ./
-docker push $tag
+docker build -t $image:$tag ./
+docker tag $image:$tag $image:latest
+docker push $image:$tag
+docker push $image:latest
